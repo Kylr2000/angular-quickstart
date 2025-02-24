b672712ea0040c65e4ad38d2f9ce65d8068d0fe7
Kyle Jaimungal <59812247+Kylr2000@users.noreply.github.com>
2025-02-24 14:40:04 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { ProjectsComponent } from './project/projects.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },  // Default page
   { path: 'resume', component: ResumeComponent },
   { path: 'project', component: ProjectsComponent },
   { path: '**', redirectTo: '' }  // Handle 404s, redirect to home
