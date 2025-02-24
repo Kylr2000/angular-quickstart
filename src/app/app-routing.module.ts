07dbc33c4e90052ad46d62b8b9ffa80bbf01fcae
Kyle Jaimungal <59812247+Kylr2000@users.noreply.github.com>
2025-02-24 14:32:59 -0400

    Update app-routing.module.ts use hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './project/projects.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: '', component: AppComponent },  // Default page
-  { 
-    path: 'resume', 
-    loadChildren: () => import('./resume/resume.module').then(m => m.ResumeModule) 
-  },
-  { 
-    path: 'project', 
-    loadChildren: () => import('./project/projects.module').then(m => m.ProjectsModule) 
-  },
-  { path: '**', redirectTo: '' }  // Redirect unknown routes to home
+  { path: 'resume', component: ResumeComponent },
+  { path: 'project', component: ProjectsComponent },
+  { path: '**', redirectTo: '' }  // Handle 404s, redirect to home
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],  // Enable hash-based routing
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
