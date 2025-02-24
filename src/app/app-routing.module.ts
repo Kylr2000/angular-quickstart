b2f16564df0bd43a36c8cd47384d1a49ddd6c2f7
Kyle Jaimungal <59812247+Kylr2000@users.noreply.github.com>
2025-02-24 14:22:46 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ResumeComponent } from './resume/resume.component';
-import { ProjectsComponent } from './project/projects.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirect default path
-  { path: 'home', loadComponent: () => import('./app.component').then(m => m.AppComponent) },
-  { path: 'resume', loadComponent: () => import('./resume/resume.component').then(m => m.ResumeComponent) },
-  { path: 'project', loadComponent: () => import('./project/projects.component').then(m => m.ProjectsComponent) },
-  { path: '**', redirectTo: 'home' } // Wildcard route for 404s
+  { path: '', component: AppComponent },  // Default page
+  { 
+    path: 'resume', 
+    loadChildren: () => import('./resume/resume.module').then(m => m.ResumeModule) 
+  },
+  { 
+    path: 'project', 
+    loadChildren: () => import('./project/projects.module').then(m => m.ProjectsModule) 
+  },
+  { path: '**', redirectTo: '' }  // Redirect unknown routes to home
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })], // Hash-based routing
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
