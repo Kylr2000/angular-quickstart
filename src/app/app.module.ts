57a086c296b6155043cf9cf5b782b543f124e656
Kyle Jaimungal <59812247+Kylr2000@users.noreply.github.com>
2025-03-10 20:29:17 -0400

    Update app.module.ts add modules for project and resume

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './project/projects.component'; 
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    ResumeComponent
+    
   ],
   imports: [
     BrowserModule,
